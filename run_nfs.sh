84aad4bf1be412d60d7a2c5ca9b11036eb36ec53
Dimitris Karakasilis <dimitris@karakasilis.me>
2024-04-15 09:27:18 +0300

    Revert "Enable nfs v4 (?)"
    This reverts commit 0447ba2bced6fce2fda707fbc5661d5a3e72b13e.

--- run_nfs.sh
+++ run_nfs.sh
@@ -54,11 +54,11 @@ function start()
 
 
     # -V 3: enable NFSv3
-    /usr/sbin/rpc.mountd -N 2 -V 3 -V 4
+    /usr/sbin/rpc.mountd -N 2 -V 3
 
     /usr/sbin/exportfs -r
     # -G 10 to reduce grace time to 10 seconds (the lowest allowed)
-    /usr/sbin/rpc.nfsd -G 10 -V 3 -V 4
+    /usr/sbin/rpc.nfsd -G 10 -V 3
     /usr/sbin/rpc.statd --no-notify
     echo "NFS started"
     showmount -e
